ad54cd534bb88e63bccca052bdd96fbfeed438ab
Mateusz Zalewski <mzalewski1992@gmail.com>
2021-11-26 15:52:41 +0100

    Script update

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ sudo apt-get install git -y
 echo "# Installing ansible"
 sudo apt install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
+sudo apt install ansible -y
 
 echo "# Fetching playbook repository..."
 git clone https://github.com/emzet92/env-setup.git
