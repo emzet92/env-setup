3018ce4bbd1ecf416bc32fbf9de29c7819ae680f
Mateusz Zalewski <mzalewski1992@gmail.com>
2021-11-26 15:45:07 +0100

    Script update

--- setup.sh
+++ setup.sh
@@ -4,12 +4,4 @@ echo "# Installing prerequisites..."
 
 sudo apt update
 
-sudo apt-get install git python3 curl python3-pip
-
-echo "# Installing pip"
-
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-sudo python3 get-pip.py
-
-echo "# Installing ansible"
-sudo python3 -m pip install ansible
+sudo apt-get install git python3 curl python3-pip ansible
