1055dfada22e52b799e48482935ed9b36bd91c3f
Mateusz Zalewski <mzalewski1992@gmail.com>
2021-11-26 15:48:01 +0100

    Script update

--- setup.sh
+++ setup.sh
@@ -4,4 +4,15 @@ echo "# Installing prerequisites..."
 
 sudo apt update
 
-sudo apt-get install git python3 curl python3-pip ansible
+sudo apt-get install git
+
+echo "# Installing ansible"
+sudo apt install software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
+
+echo "# Fetching playbook repository..."
+git clone https://github.com/emzet92/env-setup.git
+
+
+echo "DONE!!!"
